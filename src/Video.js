2fe51564e08fc6294d95ed4258f1ad7a823b9b66
Charlie Collard <charliecollard0@gmail.com>
2020-11-28 23:49:58 +0000

    Chat and video

---
+++ src/Video.js
@@ -0,0 +1,11 @@
+import './Video.css';
+
+function Video() {
+    return (
+        <iframe className="Video" src="https://www.youtube.com/embed/VJKSLTZP-Ts">
+
+        </iframe>
+    );
+}
+
+export default Video;
