8270c1e36253a46a18e52c7e1d262b2bb85932a9
Charlie Collard <charliecollard0@gmail.com>
2020-11-29 20:54:52 +0000

    Working from start

--- src/App.js
+++ src/App.js
@@ -2,117 +2,35 @@ import './App.css';
 import Video from "./Video";
 import Chat from "./Chat";
 import {useState, useEffect} from "react";
+import data from "./json/815671308.json"
 
-const initialState = [
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-    "hi",
-];
+const comments = data.comments.sort((a, b) => new Date(a.created_at) - new Date(b.created_at))
+const firstCommentTime = new Date(comments[0].created_at)
 
 function App() {
-    const [chatMessages, setChatMessages] = useState(initialState);
+    const [chatMessages, setChatMessages] = useState([]);
+    const [startTime, setStartTime] = useState(new Date());
+    const [videoTime, setVideoTime] = useState(new Date());
+    const [i, setI] = useState(0);
+
+    const updateChatMessages = () => {
+        const currentTime = new Date()
+        var messagesToAdd = []
+        var j = i
+        while ((currentTime - startTime) > (new Date(comments[j].created_at) - firstCommentTime)) {
+            messagesToAdd = messagesToAdd.concat(comments[j])
+            j += 1
+        }
+        setI(j)
+
+        const newChatMessages = chatMessages.concat(messagesToAdd)
+        const start = Math.max(newChatMessages.length - 100, 0)
+        const end = newChatMessages.length
+        setChatMessages(newChatMessages.slice(start, end));
+    }
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setChatMessages(chatMessages.concat(["hi"]));
-        }, 1000);
+        const timer = setTimeout(updateChatMessages, 1000);
         return () => clearTimeout(timer);
     })
 
--- src/Video.js
+++ src/Video.js
@@ -3,7 +3,6 @@ import './Video.css';
 function Video() {
     return (
         <iframe className="Video" src="https://www.youtube.com/embed/VJKSLTZP-Ts">
-
         </iframe>
     );
 }
