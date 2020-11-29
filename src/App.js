f990951f60bc064b94c6062e56dc8ad52a21cd38
Charlie Collard <charlie.collard@marshmallow.co>
2020-11-29 01:05:16 +0000

    Scrolling chat

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Video from "./Video";
 import Chat from "./Chat";
+import {useState, useEffect} from "react";
 
 function App() {
+    const [chatMessages, setChatMessages] = useState([]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setChatMessages(chatMessages.concat(["Hi"]));
+        }, 1000);
+        return () => clearTimeout(timer);
+    })
+
     return (
         <div className="App">
             <Video/>
-            <Chat chatMessages={["a", "b", "c"]}/>
+            <Chat chatMessages={chatMessages}/>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -10,6 +10,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
