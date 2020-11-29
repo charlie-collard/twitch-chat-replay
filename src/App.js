47a6bf9f6d99be965165651f206aa49d63bb88a2
Charlie Collard <charliecollard0@gmail.com>
2020-11-29 00:26:27 +0000

    Chat to component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Video from "./Video";
 import Chat from "./Chat";
 
 function App() {
-  return (
-    <div className="App">
-        <Video/>
-        <Chat/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Video/>
+            <Chat chatMessages={["a", "b", "c"]}/>
+        </div>
+    );
 }
 
 export default App;
--- src/Chat.js
+++ src/Chat.js
@@ -1,30 +1,16 @@
 import './Chat.css';
+import React, { FC } from 'react'
 
-function Chat() {
+type ChatProps = {
+    chatMessages: String[]
+}
+
+const Chat: FC<ChatProps> = ({ chatMessages }) => {
     return (
         <div className="Chat">
-            <iframe>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
-                <p>6</p>
-                <p>7</p>
-                <p>8</p>
-                <p>9</p>
-                <p>10</p>
-                <p>11</p>
-                <p>12</p>
-                <p>13</p>
-                <p>14</p>
-                <p>15</p>
-                <p>16</p>
-                <p>17</p>
-                <p>18</p>
-                <p>19</p>
-                <p>20</p>
-            </iframe>
+            {chatMessages.map( message => (
+                <p>{message}</p>
+            ))}
         </div>
     );
 }
