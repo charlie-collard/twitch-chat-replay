a1efe9bdf360952888eacc000b177b605e3e44e9
Charlie Collard <charlie.collard@marshmallow.co>
2021-08-08 20:54:24 +0100

    Many improvements

--- make-db.py
+++ make-db.py
@@ -10,44 +10,57 @@ create table if not exists commenters (
     id integer primary key autoincrement,
     twitchID text not null unique,
     displayName text not null,
+    name text not null,
     bio text,
+    logo text,
+    type text,
     createdAt datetime not null,
     updatedAt datetime not null
 );
-"""
-
-CREATE_COMMENTERS_INDEX = """
-create index if not exists commenters_twitch_id on commenters (twitchID);
 """
 
 CREATE_COMMENTS_TABLE = """
 create table if not exists comments (
     id integer primary key autoincrement,
     twitchID text not null unique,
-    commenterID text not null,
+    commenterID integer not null,
     channelID text not null,
     contentID text not null,
-    contentOffsetSeconds numeric not null,
+    contentOffsetSeconds integer not null,
+    body text not null,
+    fragments text not null,
+    badges text not null,
+    isAction boolean not null,
+    color text,
+    source text,
+    state text,
     createdAt datetime not null,
     updatedAt datetime not null,
-    body text not null,
-    badges text,
-    color text,
     constraint fk_comments_commenters foreign key (commenterID) references commenters (id)
 );
+"""
+
+CREATE_COMMENTS_COMMENTER_ID_INDEX = """
+create index if not exists idx_comments_commenter_id on comments (commenterID);
 """
 
 INSERT_COMMENTER = """
 insert into commenters (
     twitchID,
     displayName,
+    name,
     bio,
+    logo,
+    type,
     createdAt,
     updatedAt
 ) VALUES (
     :twitchID,
     :displayName,
+    :name,
     :bio,
+    :logo,
+    :type,
     datetime(:createdAt),
     datetime(:updatedAt)
 );
@@ -60,31 +73,32 @@ insert into comments (
     channelID,
     contentID,
     contentOffsetSeconds,
-    createdAt,
-    updatedAt,
     body,
+    fragments,
     badges,
-    color
+    isAction,
+    color,
+    source,
+    state,
+    createdAt,
+    updatedAt
 ) VALUES (
     :twitchID,
     :commenterID,
     :channelID,
     :contentID,
     :contentOffsetSeconds,
-    datetime(:createdAt),
-    datetime(:updatedAt),
     :body,
+    :fragments,
     :badges,
-    :color
+    :isAction,
+    :color,
+    :source,
+    :state,
+    datetime(:createdAt),
+    datetime(:updatedAt)
 );
 """
-
-con = sqlite3.connect("nl-chat.db")
-
-con.execute(ENABLE_FOREIGN_KEYS)
-con.execute(CREATE_COMMENTERS_TABLE)
-con.execute(CREATE_COMMENTERS_INDEX)
-con.execute(CREATE_COMMENTS_TABLE)
 
 def convert_commenter(commenter):
     if commenter is None:
@@ -92,7 +106,10 @@ def convert_commenter(commenter):
     return {
         "twitchID": commenter["_id"],
         "displayName": commenter["display_name"],
+        "name": commenter["name"],
         "bio": commenter["bio"],
+        "logo": commenter.get("logo"),
+        "type": commenter.get("type"),
         "createdAt": commenter["created_at"],
         "updatedAt": commenter["updated_at"],
     }
@@ -104,16 +121,24 @@ def convert_comment(comment, commenterID):
         "channelID": comment["channel_id"],
         "contentID": comment["content_id"],
         "contentOffsetSeconds": comment["content_offset_seconds"],
+        "body": comment["message"]["body"],
+        "fragments": json.dumps(comment["message"].get("fragments") or []),
+        "badges": json.dumps(comment["message"].get("user_badges") or []),
+        "isAction": comment["message"].get("is_action") or False,
+        "color": comment["message"].get("user_color"),
+        "source": comment["message"].get("source"),
+        "state": comment["message"].get("state"),
         "createdAt": comment["created_at"],
         "updatedAt": comment["updated_at"],
-        "body": comment["message"]["body"],
-        "badges": json.dumps(comment["message"].get("user_badges")),
-        "color": comment["message"].get("user_color")
     }
 
-with con:
+
+with sqlite3.connect("nl-chat.db") as con:
+    con.execute(ENABLE_FOREIGN_KEYS)
+    con.execute(CREATE_COMMENTERS_TABLE)
+    con.execute(CREATE_COMMENTS_TABLE)
     cur = con.cursor()
-    for filename in glob.glob("sky-videos/*.json.gz"):
+    for filename in sorted(list(glob.glob("sky-videos/*.json.gz")), key=lambda x: int(x[x.find("/")+1:x.find(".")])):
         print(filename)
         with gzip.open(filename) as f:
             data = json.load(f)
@@ -121,7 +146,6 @@ with con:
             if commenter := convert_commenter(comment["commenter"]):
                 try:
                     cur.execute(INSERT_COMMENTER, commenter)
-                    con.commit()
                     rowID = cur.lastrowid
                 except sqlite3.IntegrityError as e:
                     if str(e) == "UNIQUE constraint failed: commenters.twitchID":
@@ -135,3 +159,5 @@ with con:
                     if str(e) != "UNIQUE constraint failed: comments.twitchID":
                         raise e
         con.commit()
+
+con.execute(CREATE_COMMENTS_COMMENTER_ID_INDEX)
